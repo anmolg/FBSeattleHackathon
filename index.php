9f9dbaac165f45c7798d1a291b565a276505c9e1
Anmol Gupta <anmol@e-aviation.net>
2013-04-19 22:34:48 -0700

    gend dist test ii

--- index.php
+++ index.php
@@ -88,6 +88,7 @@ if ($user_id) {
 	foreach ($_attending as $friendse) {
 		$id = idx($friendse, 'id');
 		$friend_id = idx($facebook->api('/' . $id), 'data', array());
+		print_r($friend_id);
 		$gender = idx($friend_id, 'gender');
 		print_r($gender);
 		if ($gender == "male") {
