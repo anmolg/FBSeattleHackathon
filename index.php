f2b67f0b488cc394aa5a6c4730a57da57cbd031e
whiteSkar <sypower07@Hotmail.com>
2013-04-20 00:16:58 -0700

    testing

--- index.php
+++ index.php
@@ -126,6 +126,7 @@ if ($user_id) {
 	// for testing, right now it uses the first event
 	$picked_event = idx($events, '0');
 	$picked_event_id = idx($events, 'id');
+	print_r($picked_event);
 	print_r($picked_event_id);
 //	$attending_people_for_picked_event = idx($facebook->api('/' . $picked_event_id . '?fields=attending'), 'data', array());
 //	print_r($attending_people_for_picked_event);
