c09a6b40eb2e46d00a365f693860942090be70be
Anmol Gupta <anmol@e-aviation.net>
2013-04-19 21:06:25 -0700

    test...

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if ($user_id) {
   
   $friend_id = 0;
   
-  /*
+  
   <?php
     foreach ($friendlists as $friendk) {
               // Extract the pieces of info we need from the requests above
@@ -75,7 +75,7 @@ if ($user_id) {
 			  if ($name = "Close Friends")
 				$friend_id = $id;
 	?>	  
-					
+	/*				
 	$friends = idx($facebook->api('/' . $friend_id . '/members'), 'data', array());
 	*/		 
 	
